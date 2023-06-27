04bcfa9d8d90e8d33ede6426a0ec496f0fa837d1
Jonalinux <103053714+jonalinux@users.noreply.github.com>
2023-06-27 10:15:41 +0100

    Update clamav.sh

--- scripts/clamav.sh
+++ scripts/clamav.sh
@@ -7,17 +7,17 @@
 
 # Set Scan
 weekday="Tue" # see date +'%d'
-dirscan="/home/jonathan/.scripts/avtest" # directory of scan
+dirscan="/path" # directory of scan
 
 #variable
 data=$(date +'%d')
 target="$weekday"
 current=$(date +'%a')
-log="$HOME/.scripts/av/log" # log file
-url="$HOME/.scripts/av/last" # date of the last scan
+log="$HOME/scripts/av/log" # log file
+url="$HOME/scripts/av/last" # date of the last scan
 last=$(cat $url)
-icon1="$HOME/.scripts/av/img/icon1.svg"
-icon2="$HOME/.scripts/av/img/icon2.svg"
+icon1="$HOME/scripts/av/img/icon1.svg"
+icon2="$HOME/scripts/av/img/icon2.svg"
 
 
 
