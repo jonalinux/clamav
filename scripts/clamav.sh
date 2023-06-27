30682a3b92f893efd2f89d3caa9f9c269ce077f1
Jonalinux <103053714+jonalinux@users.noreply.github.com>
2023-06-27 02:53:38 +0100

    Update clamav.sh

--- scripts/clamav.sh
+++ scripts/clamav.sh
@@ -12,10 +12,10 @@ dirscan="/home/$user/"
 
 #variable
 data=$(date +'%d')
-last=$(cat $HOME/.scripts/av/last)
+last=$(cat $HOME/scripts/av/last)
 target="$weekday"
 current=$(date +'%a')
-log="$HOME/.scripts/av/log"
+log="$HOME/scripts/av/log"
 
 clamav(){
 
@@ -23,7 +23,7 @@ if [ "$target" = "$current" ]; then
     if ! [ "$data" -eq "$last" ]; then 
          echo "scan!"
          nice -n19 clamscan -ri --remove=yes  $dirscan > "$log";
-         echo "$data" > $HOME/.scripts/av/last
+         echo "$data" > $HOME/scripts/av/last
          MALWARE=$(tail "$log" | grep 'Infected' | cut -d " " -f3);
          if [ "$MALWARE" -eq "0" ];then
              notify-send  "ClamAV"  "System Protect"   -u normal
