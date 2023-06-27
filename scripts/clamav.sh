f69e72d6e288c863225cd4f53b02729a5803a782
Jonalinux <103053714+jonalinux@users.noreply.github.com>
2023-06-27 10:14:27 +0100

    Add files via upload

--- scripts/clamav.sh
+++ scripts/clamav.sh
@@ -3,32 +3,55 @@
 # Author : Jonathan Sanfilippo
 # Date: Jun 2023
 # Version 1.0.0: clamav weekly script
-#
-
+# Tested with www.eicar.org
 
 # Set Scan
-weekday="Tue"
-dirscan="/home/$user/"
+weekday="Tue" # see date +'%d'
+dirscan="/home/jonathan/.scripts/avtest" # directory of scan
 
 #variable
 data=$(date +'%d')
-last=$(cat $HOME/scripts/av/last)
 target="$weekday"
 current=$(date +'%a')
-log="$HOME/scripts/av/log"
+log="$HOME/.scripts/av/log" # log file
+url="$HOME/.scripts/av/last" # date of the last scan
+last=$(cat $url)
+icon1="$HOME/.scripts/av/img/icon1.svg"
+icon2="$HOME/.scripts/av/img/icon2.svg"
+
+
 
 clamav(){
 
-if [ "$target" = "$current" ]; then 
-    if ! [ "$data" -eq "$last" ]; then 
+if [ "$target" = "$current" ]; then # check if it is the day of the week to scan
+    if ! [ "$data" -eq "$last" ]; then # Compare today's date with the last scan
+         
          echo "scan!"
-         nice -n19 clamscan -ri --remove=yes  $dirscan > "$log";
-         echo "$data" > $HOME/scripts/av/last
+         nice -n19  clamscan -ri  --remove=yes $dirscan > "$log";
+         echo "$data" > $url
+         
          MALWARE=$(tail "$log" | grep 'Infected' | cut -d " " -f3);
          if [ "$MALWARE" -eq "0" ];then
-             notify-send  "ClamAV"  "System Protect"   -u normal
+         
+             ACTION=$(notify-send -i "$icon1" --action="Read Log" --action="Close"  "ClamAV"  "System Protect"   -u normal  -u critical )
+                case "$ACTION" in
+                      "0")
+                         vim $log
+                         ;;
+                      "1")
+                         exit
+                         ;;
+                esac
          else
-             notify-send  "ClamAV"  "$MALWARE Malware Removed"   -u critical
+             ACTION=$(notify-send -i "$icon2" --action="Read log" --action="Close"  "ClamAV"  "$MALWARE Malware Removed"  -u critical )
+                case "$ACTION" in
+                      "0")
+                         vim $log
+                         ;;
+                      "1")
+                         exit
+                         ;;
+                esac
          fi    
     else
         echo "already scan!"
